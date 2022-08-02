8ca1305df1af84de3ca01b06d237c57e02205f04
Dillon-Roller <dillonroller.18@gmail.com>
2022-08-02 14:29:13 -0400

    reverse price data for time correction

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from Stock import Stock
 
-# stock = Stock("BTCUSD")
-stock = Stock("SPY")
+stock = Stock("BTCUSD")
+# stock = Stock("SPY")
 stock.plot()
+
+# Time is in reverse order
+for i in stock.prices.__reversed__():
+    print(i)
