d39991beacfd3e3fbe774315fe408a76bf8f6716
Daniel Albl <daniel.albl@vivint.com>
2022-08-02 22:58:49 -0600

    Add exponential moving average

--- Stock.py
+++ Stock.py
@@ -21,7 +21,7 @@ class Stock:
         if not os.path.isdir(JSON_DIR):
             os.mkdir(JSON_DIR)
 
-        if update or not os.path.exists(ticker + ".json"):
+        if update or not os.path.exists(self.file):
             self.update()
         else:
             self.read()
@@ -82,13 +82,36 @@ class Stock:
         return sma
         
     #TODO: code and optimize
-    def _get_EMA_prices(self, period, k):
-        pass
+    def get_EMA_prices(self, period=200):
+        k = 2.0 / (period + 1)
+
+        ema = SortedDict()
+
+        dates = list(self.prices.keys())
+        prices = list(self.prices.values())
+
+        size = len(dates)
+
+        sum = 0.0
+        for i in range(size):
+            if i < period:
+                sum += prices[i]
+
+                if i == period - 1:
+                    ema[dates[i]] = sum / period
+            else:
+                ema[dates[i]] = k * prices[i] + (1.0 - k) * ema[dates[i-1]]
+
+        self.indicators["ema"] = ema
+
+        return ema
 
     def plot(self):
         plt.plot(self.prices.keys(), self.prices.values(), label=self.ticker)
 
         for label, data in self.indicators.items():
             plt.plot(data.keys(), data.values(), label=label)
+
+        plt.legend(loc='best', prop={'size': 20})
 
         plt.show()
--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from Stock import Stock
 
 # stock = Stock("BTCUSD")
 stock = Stock("SPY")
+stock.get_EMA_prices()
 stock.get_SMA_prices()
 stock.plot()
