9fcb4aeb156396c24cda61a8b6f5a381b106f25b
Daniel Albl <daniel.albl@vivint.com>
2022-08-05 20:13:32 -0600

    Implement variable timeframe for alphavantage API

--- Stock.py
+++ Stock.py
@@ -7,15 +7,37 @@ import requests
 
 API_KEY = "25R4JNO0E4GWP3RI"
 ENDPOINT = "https://www.alphavantage.co/query"
-DAILY_DATA_FUNC = "TIME_SERIES_DAILY"
 OUTPUT_SIZE = "full"
+DATATYPE = "json"
 JSON_DIR = "json"
 
+FUNC_STR = "func"
+INTERVAL_STR = "interval"
+DATE_FORMAT = "date_format"
+
+API_ARGS = {
+    "1m": {FUNC_STR: "TIME_SERIES_INTRADAY", INTERVAL_STR: "1min", DATE_FORMAT: "%Y-%m-%d %X"},
+    "5m": {FUNC_STR: "TIME_SERIES_INTRADAY", INTERVAL_STR: "5min", DATE_FORMAT: "%Y-%m-%d %X"},
+    "15m": {FUNC_STR: "TIME_SERIES_INTRADAY", INTERVAL_STR: "15min", DATE_FORMAT: "%Y-%m-%d %X"},
+    "30m": {FUNC_STR: "TIME_SERIES_INTRADAY", INTERVAL_STR: "30min", DATE_FORMAT: "%Y-%m-%d %X"},
+    "1h": {FUNC_STR: "TIME_SERIES_INTRADAY", INTERVAL_STR: "60min", DATE_FORMAT: "%Y-%m-%d %X"},
+    "D": {FUNC_STR: "TIME_SERIES_DAILY", INTERVAL_STR: None, DATE_FORMAT: "%Y-%m-%d"},
+    "W": {FUNC_STR: "TIME_SERIES_WEEKLY", INTERVAL_STR: None, DATE_FORMAT: "%Y-%m-%d"},
+    "M": {FUNC_STR: "TIME_SERIES_MONTHLY", INTERVAL_STR: None, DATE_FORMAT: "%Y-%m-%d"}
+}
+
 class Stock:
-    def __init__(self, ticker, update=False):
+    def __init__(self, ticker, timeframe="D", update=False):
+        if timeframe not in API_ARGS:
+            raise Exception(f"{timeframe} is not a valid timeframe!")
+
         self.ticker = ticker
-        self.file = JSON_DIR + "/" + ticker + ".json"
+        self.file = JSON_DIR + "/" + ticker + "-" + timeframe + ".json"
         self.indicators = {}
+        self.api_func = API_ARGS[timeframe][FUNC_STR]
+        self.api_interval = API_ARGS[timeframe][INTERVAL_STR]
+        self.date_format = API_ARGS[timeframe][DATE_FORMAT]
+        self.timeframe = timeframe
 
         if not os.path.isdir(JSON_DIR):
             os.mkdir(JSON_DIR)
@@ -26,23 +48,24 @@ class Stock:
         else:
             json_ = self.read_json_from_files()
 
-        self.__get_attrs_from_json(json_)
-
-
-
+        self.__load_attrs_from_json(json_)
 
     def __get_prices(self):
         prices = SortedDict()
 
         for date, info in self.historical.items():
-            date = datetime.strptime(date, "%Y-%m-%d")
+            date = datetime.strptime(date, self.date_format)
             price = float(info["4. close"])
             prices[date] = price
 
         return prices
 
-    def __get_attrs_from_json(self, json_):
-        self.historical = json.loads(json_)["Time Series (Daily)"]
+    def __load_attrs_from_json(self, json_):
+        data = json.loads(json_)
+        # get key for historical data
+        # there is only one other key besides 'Meta Data'
+        key = [key for key in data.keys() if key != "Meta Data"][0]
+        self.historical = data[key]
         self.prices = self.__get_prices()
 
     def read_json_from_files(self):
@@ -54,7 +77,14 @@ class Stock:
             f.write(json_)
 
     def get_json_from_api(self):
-        params = {"function": DAILY_DATA_FUNC, "symbol": self.ticker, "outputsize": OUTPUT_SIZE, "apikey": API_KEY}
+        params = {
+            "function": self.api_func,
+            "symbol": self.ticker,
+            "interval": self.api_interval,
+            "outputsize": OUTPUT_SIZE,
+            "apikey": API_KEY,
+            "datatype": DATATYPE
+        }
         return requests.get(ENDPOINT, params).text
 
      # TODO: optimize with numpy?
--- main.py
+++ main.py
@@ -1,22 +1,9 @@
 from Stock import Stock
 
 # stock = Stock("ETHUSD")
-stock = Stock("SPY")
+stock = Stock("ETHUSD", timeframe="D", update=True)
 
-size = len(stock.prices.values())
-
-winning_periods = 0.0
-
-lump_sum = stock.lump_sum()
-
-for i in range(1, size // 2):
-    dca = stock.dollar_cost_average(i)
-    if dca > lump_sum:
-        winning_periods += 1
-
-win_ratio = winning_periods / (size // 2)
-
-print("Percent Win: " + str(100 * win_ratio))
+stock.plot()
 
 
 
