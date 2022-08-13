5609bc028afe28df291de729fb904c04292c4c8a
Dillon-Roller <dillonroller.18@gmail.com>
2022-08-13 17:43:13 -0400

    add more variable and update_diversification logic

--- Investment.py
+++ Investment.py
@@ -3,4 +3,11 @@ from Asset import Asset
 class Investment:
     def __init__(self, ticker, timeframe, length):
       self.asset = Asset(ticker, timeframe, length)
+      self.equity = None
+      self.shares = None
+      self.average_cost = None
+      self.diversity = None
+
+    def update_diversification(self, total_equity):
+        self.diversity = total_equity / self.equity
 
--- Portfolio.py
+++ Portfolio.py
@@ -5,11 +5,16 @@ class Portfolio:
         self.investments= {}
         self.timeframe = None
         self.length = None
-    
+        self.total_equity = None
+
     def buy_asset(self, ticker, amount):
         if ticker not in self.investments:
             self.investments[ticker] = Investment(ticker, self.timeframe, self.length)
-        pass
 
+
+    def update_diversifications(self):
+        for investment in self.investments.values():
+            investment.update_diversification(self.total_equity)
+            
     def set_timeframe(self, timeframe):
         self.timeframe = timeframe
