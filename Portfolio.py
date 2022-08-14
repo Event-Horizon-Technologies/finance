6234adcf0bca00511d9a4c15bdc2dc91f4c44718
Dillon-Roller <dillonroller.18@gmail.com>
2022-08-14 15:18:13 -0400

    allow buying and selling multiple assets at a time

--- Portfolio.py
+++ Portfolio.py
@@ -8,24 +8,27 @@ class Portfolio:
         self.length = None
         self.total_equity = 0.0
 
-    def buy_asset(self, symbol, date, amount):
-        if symbol not in self.investments:
-            self.investments[symbol] = Investment(symbol, self.timeframe, self.length)
-
-        self.investments[symbol].buy(date, amount)
+    def buy_assets(self, symbols, dates, amounts):
+        for symbol, date, amount in symbols, dates, amounts:
+            if symbol not in self.investments:
+                self.investments[symbol] = Investment(symbol, self.timeframe, self.length)
+                self.investments[symbol].buy(date, amount)
+        
         self.__update_attributes()
 
-    def sell_asset(self, symbol, date, amount):
-        if symbol in self.investments:
-            if not self.investments[symbol].sell(date, amount):
-                warn(f"Attempted to sell more {symbol} than you own, ignored")
+    def sell_assets(self, symbols, dates, amounts):
+        for symbol, date, amount in symbols, dates, amounts:
+            if symbol in self.investments:
+                if not self.investments[symbol].sell(date, amount):
+                    warn(f"Attempted to sell more {symbol} than you own, ignored")
 
-            if self.investments[symbol].get_equity() <= 0:
-                del self.investments[symbol]
-            
-            self.__update_attributes()
-        else:
-            warn(f"Attempted to sell {symbol} without owning any, ignored")
+                if self.investments[symbol].get_equity() <= 0:
+                    del self.investments[symbol]
+                
+            else:
+                warn(f"Attempted to sell {symbol} without owning any, ignored")
+                
+        self.__update_attributes()
         
     def __update_attributes(self):
         self.__update_total_equity()
