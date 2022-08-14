95fc007ba704237e4158d18231635e20ac6fc04f
Dillon-Roller <dillonroller.18@gmail.com>
2022-08-13 23:19:34 -0400

    implement update_attributes

--- Portfolio.py
+++ Portfolio.py
@@ -8,29 +8,32 @@ class Portfolio:
         self.length = None
         self.total_equity = None
 
-    def buy_asset(self, symbol, amount):
+    def buy_asset(self, symbol, amount):    
         if symbol not in self.investments:
             self.investments[symbol] = Investment(symbol, self.timeframe, self.length)
 
         self.investments[symbol].buy(amount)
+        self.__update_attributes()
 
     def sell_asset(self, symbol, amount):
         if symbol in self.investments:
             self.investments[symbol].sell(amount)
+            self.__update_attributes()
         else:
             warn(f"Attempted to sell {symbol} without owning any, ignored")
+        
 
+    def __update_attributes(self):
+        self.__update_total_equity()
+        self.__update_diversifications()
 
     def __update_diversifications(self):
-        # should this function set self.total_equity or let this be done elsewhere ?
-        self.total_equity = self.__calc_total_equity()
-
         for investment in self.investments.values():
             investment.update_diversification(self.total_equity)
 
-    def __calc_total_equity(self):
-        return sum([investment.get_equity() for investment in self.investments.values()])
-
+    def __update_total_equity(self):
+        self.total_equity = sum([investment.get_equity() for investment in self.investments.values()])
+    
     def set_timeframe(self, timeframe):
         self.timeframe = timeframe
 
