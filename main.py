16518490a75fbcb2308a788da72f4fe5b0503089
Dillon-Roller <dillonroller.18@gmail.com>
2022-08-28 19:52:32 -0400

    main changes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 from datetime import datetime, timedelta
 from Simulator import Simulator
 from Strategy import *
@@ -5,25 +7,33 @@ import Indicators
 from Asset import Asset
 import matplotlib.pyplot as plt
 from pytz import UTC
-
-SYMBOL = "NVDA"
-TIMEFRAME = "5m"
+import sys
 
 
-asset = Asset(SYMBOL, timeframe=TIMEFRAME)
 
-start = asset.close.start_date
-end = asset.close.end_date - 2 * asset.close.interval
+def main(argv):
+    SYMBOL    = argv[0] if len(argv) > 0 else "NVDA"
+    TIMEFRAME = argv[1] if len(argv) > 1 else "5m"
 
-# start = datetime.strptime("2021-01-01", "%Y-%m-%d").replace(tzinfo=UTC)
-# end = datetime.strptime("2022-01-01", "%Y-%m-%d").replace(tzinfo=UTC)
+    asset = Asset(SYMBOL, timeframe=TIMEFRAME)
 
-cash = asset.get_price_by_date(start)
+    start = asset.close.start_date
+    end = asset.close.end_date - 2 * asset.close.interval
 
-s = Simulator(start_date=start, end_date=end, timeframe=TIMEFRAME, cash=cash)
-s.run(PSAR_EMA(SYMBOL))
-s.plot(plot_assets=True)
+    # start = datetime.strptime("2021-01-01", "%Y-%m-%d").replace(tzinfo=UTC)
+    # end = datetime.strptime("2022-01-01", "%Y-%m-%d").replace(tzinfo=UTC)
 
-print(s.get_alpha())
+    cash = asset.get_price_by_date(start)
+
+    s = Simulator(start_date=start, end_date=end, timeframe=TIMEFRAME, cash=cash)
+    s.run(PSAR_EMA(SYMBOL))
+    s.plot(plot_assets=True)
+
+    print(s.get_alpha())
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
 
 
