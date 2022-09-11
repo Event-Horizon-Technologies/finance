ef92578eaac537ce0088ce8ae3675e1ab2008b72
Dillon-Roller <dillonroller.18@gmail.com>
2022-09-11 19:01:18 -0400

    test_spy_1d

--- test/test_Asset.py
+++ test/test_Asset.py
@@ -12,3 +12,10 @@ class TestAsset(TestBaseClass):
 
         self.run(open=asset.open, close=asset.close, high=asset.high, low=asset.low)
 
+    def test_spy_1d(self):
+        start_date = np.datetime64("2014-09-17", Utils.DATETIME_TYPE)
+        end_date = np.datetime64("2022-09-06", Utils.DATETIME_TYPE)
+        asset = Asset(symbol="SPY", timeframe="1d", start_date=start_date, end_date=end_date)
+
+        self.run(open=asset.open, close=asset.close, high=asset.high, low=asset.low)
+
