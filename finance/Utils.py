f5eda0c1fb5cfc7fafd62d933bd2670da0a4a948
Dillon-Roller <dillonroller.18@gmail.com>
2022-09-11 19:41:37 -0400

    add read/write files to Utils

--- finance/Utils.py
+++ finance/Utils.py
@@ -9,3 +9,11 @@ def show_plot():
 
 def create_np_datetime(timestamp):
     return timestamp.to_datetime64().astype(f"datetime64[{DATETIME_TYPE}]")
+
+def write_to_file(file_name, data):
+    with open(file_name, 'w') as f:
+        f.write(str(data))
+
+def read_from_file(file_name, data):
+    with open(file_name, 'r') as f:
+        return f.read()
