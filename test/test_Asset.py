d464aa0a860c81cc78326e4798a13053b95b9d93
Dillon-Roller <dillonroller.18@gmail.com>
2022-09-11 20:07:37 -0400

    test_btc_1h bugged

--- test/test_Asset.py
+++ test/test_Asset.py
@@ -12,6 +12,13 @@ class TestAsset(TestBaseClass):
 
         self.run(open=asset.open, close=asset.close, high=asset.high, low=asset.low)
 
+    def test_btc_1h(self):
+        start_date = np.datetime64("2021-09-06", Utils.DATETIME_TYPE)
+        end_date = np.datetime64("2022-09-06", Utils.DATETIME_TYPE)
+        asset = Asset(symbol="BTC-USD", timeframe="1h", start_date=start_date, end_date=end_date)
+
+        self.run(open=asset.open, close=asset.close, high=asset.high, low=asset.low)
+
     def test_spy_1d(self):
         start_date = np.datetime64("1993-01-29", Utils.DATETIME_TYPE)
         end_date = np.datetime64("2022-09-06", Utils.DATETIME_TYPE)
