9ca34775d145dc4b5e14b23f3e2c2a708bef4537
Daniel Albl <daniel.albl@vivint.com>
2022-09-25 15:53:35 -0600

    Allow saving data

--- finance/Trainer.py
+++ finance/Trainer.py
@@ -1,16 +1,19 @@
 from finance.Asset import Asset
-from finance import Utils
 
 import numpy as np
+import pickle
 import tensorflow as tf
 from tensorflow import keras
 
 class Trainer:
     def __init__(self):
-        pass
+        self.input_data = self.target_data = None
 
-    @staticmethod
-    def generate_data(symbols, indicators, timeframe="1d", prediction_offset=30):
+    @property
+    def input_size(self):
+        return self.input_data.shape[1] if self.input_data else 0
+
+    def generate_data(self, symbols, indicators, timeframe="1d", prediction_offset=30):
         input_data = []; target_data = []
 
         for symbol in symbols:
@@ -20,7 +23,16 @@ class Trainer:
             input_data.append(np.column_stack([i.create_neural_net_data(asset, prediction_offset) for i in indicators]))
             target_data.append(np.log(prices[prediction_offset:] / prices[:-prediction_offset]))
 
-        return np.concatenate(input_data), np.concatenate(target_data)
+        self.input_data, self.target_data = np.concatenate(input_data), np.concatenate(target_data)
 
+    def load_data(self, path_to_data):
+        with open(path_to_data.joinpath("input_data.pickle"), "rb") as f:
+            self.input_data = pickle.load(f)
+        with open(path_to_data.joinpath("target_data.pickle"), "rb") as f:
+            self.target_data = pickle.load(f)
 
-
+    def save_data(self, path_to_data):
+        with open(path_to_data.joinpath("input_data.pickle"), "wb") as f:
+            pickle.dump(self.input_data, f)
+        with open(path_to_data.joinpath("target_data.pickle"), "wb") as f:
+            pickle.dump(self.target_data, f)
