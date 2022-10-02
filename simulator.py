7c6d105e3a9179728e356e9674a6d447b83295b5
Dillon Roller <dillonroller.18@gmail.com>
2022-10-02 01:29:47 -0400

    replace main.py with more specific files right now

---
+++ simulator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+from finance import Indicators, Simulator, Strategy, Asset, Utils
+
+import numpy as np
+import sys
+
+def main(argv):
+    symbol    = argv[0] if len(argv) > 0 else "BTC-USD"
+    timeframe = argv[1] if len(argv) > 1 else "1d"
+
+    asset = Asset(symbol, timeframe=timeframe)
+
+    start = asset.close.start_date
+    end = asset.close.end_date
+
+    cash = asset.get_price_by_date(start)
+
+    s = Simulator(strategy=Strategy.PSAR_EMA(symbol), start_date=start, end_date=end, timeframe=timeframe, cash=cash)
+    s.run()
+    s.plot(plot_assets=True)
+
+    print(s.get_alpha())
+
+
+if __name__ == "__simulator__":
+    main(sys.argv[1:])
