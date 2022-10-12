702b6d9a71726b8c8e26d64b71e60b1d3ee2d2e7
Daniel Albl <daniel.albl@vivint.com>
2022-10-11 20:04:59 -0600

    Make functions for getting stock and crypto pandas

--- finance/AlphaVantage.py
+++ finance/AlphaVantage.py
@@ -10,6 +10,20 @@ SYMBOL = "TSLA"
 INTERVAL = "5min"
 API_KEY = "ZZ7E9KFFYTKGZ0XR"
 CRYPTO_LIST = Path(__file__).parent.joinpath("crypto_list.txt")
+DATATYPE = "csv"
+MARKET = "USD"
+
+def is_crypto(symbol):
+    with open(CRYPTO_LIST) as f:
+        for line in f.readlines():
+            if symbol == line.strip():
+                return True
+    return False
+
+def pandas_request(url):
+    response = requests.get(url).content.decode("UTF-8")
+    return pandas.read_csv(StringIO(response))
+
 
 """finance AlphaVantageAPI wrapper class"""
 class AlphaVantage(Static):
@@ -18,7 +32,7 @@ class AlphaVantage(Static):
     
     """Core Stock API start"""
     @staticmethod
-    def get_intraday_request(symbol, interval, adjusted="true", outputsize="compact", datatype="csv"):
+    def get_intraday_request(symbol, interval, adjusted="true", outputsize="compact", datatype=DATATYPE):
         return requests.get(
             f"{URL_BASE}function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}&outputsize={outputsize}&datatype={datatype}&apikey={API_KEY}"
         )
@@ -30,43 +44,43 @@ class AlphaVantage(Static):
         )
 
     @staticmethod
-    def get_daily_request(symbol, outputsize="compact", datatype="csv"):
+    def get_daily_request(symbol, outputsize="compact", datatype=DATATYPE):
         return requests.get(
             f"{URL_BASE}function=TIME_SERIES_DAILY&symbol={symbol}&outputsize={outputsize}&datatype={datatype}&apikey={API_KEY}"
         )
 
     @staticmethod
-    def get_daily_adjusted_request(symbol, outputsize="compact", datatype="csv"):
+    def get_daily_adjusted_request(symbol, outputsize="compact", datatype=DATATYPE):
         return requests.get(
             f"{URL_BASE}function=TIME_SERIES_DALY_ADJUSTED&symbol={symbol}&outputsize={outputsize}&datatype={datatype}&apikey={API_KEY}"
         )
 
     @staticmethod
-    def get_weekly_request(symbol, datatype="csv"):
+    def get_weekly_request(symbol, datatype=DATATYPE):
         return requests.get(
             f"{URL_BASE}function=TIME_SERIES_WEEKLY&symbol={symbol}&datatype={datatype}&apikey={API_KEY}"
         )
 
     @staticmethod
-    def get_monthly_request(symbol, datatype="csv"):
+    def get_monthly_request(symbol, datatype=DATATYPE):
         return requests.get(
             f"{URL_BASE}function=TIME_SERIES_MONTHLY&symbol={symbol}&datatype={datatype}&apikey={API_KEY}"
         )
 
     @staticmethod
-    def get_monthly_adjusted_request(symbol, datatype="csv"):
+    def get_monthly_adjusted_request(symbol, datatype=DATATYPE):
         return requests.get(
             f"{URL_BASE}function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={symbol}&datatype={datatype}&apikey={API_KEY}"
         )
 
     @staticmethod
-    def get_quote_endpoint_request(symbol, datatype="csv"):
+    def get_quote_endpoint_request(symbol, datatype=DATATYPE):
         return requests.get(
             f"{URL_BASE}function=GLOBAL_QUOTE&symbol={symbol}&datatype={datatype}&apikey={API_KEY}"
         )
     
     @staticmethod
-    def get_search_endpoint_request(keywords, datatype="csv"):
+    def get_search_endpoint_request(keywords, datatype=DATATYPE):
         return requests.get(
             f"{URL_BASE}function=SYMBOL_SEARCH&keywords={keywords}&datatype={datatype}&apikey={API_KEY}"
         )
@@ -86,31 +100,16 @@ class AlphaVantage(Static):
         )
     """Alpha Intelligence end"""
 
-def is_crypto(symbol):
-    with open(CRYPTO_LIST) as f:
-        for line in f.readlines():
-            if symbol == line.strip():
-                return True
-    return False
+    @staticmethod
+    def get_stock_data(symbol, interval, datatype=DATATYPE, outputsize="full"):
+        function = "TIME_SERIES_DAILY" if interval == "1day" else "TIME_SERIES_INTRADAY_EXTENDED"
+        url = f"{URL_BASE}function={function}&symbol={symbol}&apikey={API_KEY}&datatype={datatype}&outputsize={outputsize}"
+        if interval != "1day": url += f"&interval={interval}"
+        return pandas_request(url)
 
-def get_function(symbol, interval):
-    if is_crypto(symbol):
-        return "DIGITAL_CURRENCY_DAILY" if interval == "1day" else "CRYPTO_INTRADAY"
-    return "TIME_SERIES_DAILY" if interval == "1day" else "TIME_SERIES_INTRADAY_EXTENDED"
-
-
-def call(symbol, interval):
-    url = f"{URL_BASE}symbol={symbol}&apikey={API_KEY}&datatype=csv"
-    if is_crypto(symbol):
-        url += f"&function={'DIGITAL_CURRENCY_DAILY' if interval == '1day' else 'CRYPTO_INTRADAY'}&market=USD" 
-    else:
-        url += f"&function={'TIME_SERIES_DAILY' if interval == '1day' else 'TIME_SERIES_INTRADAY_EXTENDED'}&outputsize=full"
-        
-    if interval != "1day":
-        url += f"&interval={interval}"
-
-    return requests.get(url)
-
-
-response = call(SYMBOL, INTERVAL).content.decode("UTF-8")
-print(pandas.read_csv(StringIO(response)))
+    @staticmethod
+    def get_crypto_data(symbol, interval, datatype=DATATYPE, market=MARKET):
+        function = "DIGITAL_CURRENCY_DAILY" if interval == "1day" else "CRYPTO_INTRADAY"
+        url = f"{URL_BASE}function={function}&symbol={symbol}&apikey={API_KEY}&datatype={datatype}&market={market}"
+        if interval != "1day": url += f"&interval={interval}"
+        return pandas_request(url)
