96a13c0552b246aa2e4f5b71e2a7135cc9e9cd47
Dillon Roller <dillonroller.18@gmail.com>
2022-10-14 13:58:47 -0400

    Use pandas_request

--- finance/AlphaVantage.py
+++ finance/AlphaVantage.py
@@ -13,6 +13,7 @@ CRYPTO_LIST = Path(__file__).parent.joinpath("crypto_list.txt")
 DATATYPE = "csv"
 MARKET = "USD"
 
+"""Move these to separate namespace or include in a classs?"""
 def is_crypto(symbol):
     with open(CRYPTO_LIST) as f:
         for line in f.readlines():
@@ -32,56 +33,56 @@ class AlphaVantage(Static):
     
     """Core Stock API start"""
     @staticmethod
-    def get_intraday_request(symbol, interval, adjusted="true", outputsize="compact", datatype=DATATYPE):
-        return requests.get(
+    def get_intraday_stock(symbol, interval, adjusted="true", outputsize="compact", datatype=DATATYPE):
+        return pandas_request(
             f"{URL_BASE}function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}&outputsize={outputsize}&datatype={datatype}&apikey={API_KEY}"
         )
     
     @staticmethod
-    def get_intraday_extended_request(symbol, interval, slice="year1month1", adjusted="true"):
-        return requests.get(
+    def get_intraday_extended_stock(symbol, interval, slice="year1month1", adjusted="true"):
+        return pandas_request(
             f"{URL_BASE}function=TIME_SERIES_INTRADAY_EXTENDED&symbol={symbol}&interval={interval}&slice={slice}&adjusted={adjusted}&apikey={API_KEY}"
         )
 
     @staticmethod
-    def get_daily_request(symbol, outputsize="compact", datatype=DATATYPE):
-        return requests.get(
+    def get_daily_stock(symbol, outputsize="compact", datatype=DATATYPE):
+        return pandas_request(
             f"{URL_BASE}function=TIME_SERIES_DAILY&symbol={symbol}&outputsize={outputsize}&datatype={datatype}&apikey={API_KEY}"
         )
 
     @staticmethod
     def get_daily_adjusted_request(symbol, outputsize="compact", datatype=DATATYPE):
-        return requests.get(
+        return pandas_request(
             f"{URL_BASE}function=TIME_SERIES_DALY_ADJUSTED&symbol={symbol}&outputsize={outputsize}&datatype={datatype}&apikey={API_KEY}"
         )
 
     @staticmethod
     def get_weekly_request(symbol, datatype=DATATYPE):
-        return requests.get(
+        return pandas_request(
             f"{URL_BASE}function=TIME_SERIES_WEEKLY&symbol={symbol}&datatype={datatype}&apikey={API_KEY}"
         )
 
     @staticmethod
     def get_monthly_request(symbol, datatype=DATATYPE):
-        return requests.get(
+        return pandas_request(
             f"{URL_BASE}function=TIME_SERIES_MONTHLY&symbol={symbol}&datatype={datatype}&apikey={API_KEY}"
         )
 
     @staticmethod
     def get_monthly_adjusted_request(symbol, datatype=DATATYPE):
-        return requests.get(
+        return pandas_request(
             f"{URL_BASE}function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={symbol}&datatype={datatype}&apikey={API_KEY}"
         )
 
     @staticmethod
     def get_quote_endpoint_request(symbol, datatype=DATATYPE):
-        return requests.get(
+        return pandas_request(
             f"{URL_BASE}function=GLOBAL_QUOTE&symbol={symbol}&datatype={datatype}&apikey={API_KEY}"
         )
     
     @staticmethod
     def get_search_endpoint_request(keywords, datatype=DATATYPE):
-        return requests.get(
+        return pandas_request(
             f"{URL_BASE}function=SYMBOL_SEARCH&keywords={keywords}&datatype={datatype}&apikey={API_KEY}"
         )
     """Core Stock API end"""
@@ -89,13 +90,13 @@ class AlphaVantage(Static):
     """Alpha Intelligence start"""
     @staticmethod
     def get_news_and_sentiments(tickers="", topics="", time_from="", time_to="", sort="LATEST", limit="50"):
-        return requests.get(
+        return pandas_request(
             f"{URL_BASE}function=NEWS_SENTIMENT&tickers={tickers}&topics={topics}&time_from={time_from}&time_to={time_to}&sort={sort}&limit={limit}&apikey={API_KEY}"
         )
     
     @staticmethod
     def get_winning_portfolios(season):
-        return requests.get(
+        return pandas_request(
             f"{URL_BASE}function=TOURNAMENT_PORTFOLIO&season={season}&apikey={API_KEY}"
         )
     """Alpha Intelligence end"""
