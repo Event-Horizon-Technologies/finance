14879a676b886fba638b6241aac2adb646e7158a
Dillon Roller <dillonroller.18@gmail.com>
2022-10-15 20:45:01 -0400

    Use constants

--- finance/AlphaVantage.py
+++ finance/AlphaVantage.py
@@ -11,9 +11,11 @@ INTERVAL = "5min"
 API_KEY = "ZZ7E9KFFYTKGZ0XR"
 CRYPTO_LIST = Path(__file__).parent.joinpath("crypto_list.txt")
 DATATYPE = "csv"
+OUTPUTSIZE = "full"
+ADJUSTED = "true"
 MARKET = "USD"
 
-"""Move these to separate namespace or include in a classs?"""
+"""Move these to separate namespace or include in a class?"""
 def is_crypto(symbol):
     with open(CRYPTO_LIST) as f:
         for line in f.readlines():
@@ -33,25 +35,25 @@ class AlphaVantage(Static):
     
     """Core Stock API start"""
     @staticmethod
-    def get_intraday_stock(symbol, interval, adjusted="true", outputsize="compact", datatype=DATATYPE):
+    def get_intraday_stock(symbol, interval, adjusted=ADJUSTED, outputsize=OUTPUTSIZE, datatype=DATATYPE):
         return pandas_read_csv(
             f"{URL_BASE}function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}&outputsize={outputsize}&datatype={datatype}&apikey={API_KEY}"
         )
     
     @staticmethod
-    def get_intraday_extended_stock(symbol, interval, slice="year1month1", adjusted="true"):
+    def get_intraday_extended_stock(symbol, interval, slice="year1month1", adjusted=ADJUSTED):
         return pandas_read_csv(
             f"{URL_BASE}function=TIME_SERIES_INTRADAY_EXTENDED&symbol={symbol}&interval={interval}&slice={slice}&adjusted={adjusted}&apikey={API_KEY}"
         )
 
     @staticmethod
-    def get_daily_stock(symbol, outputsize="compact", datatype=DATATYPE):
+    def get_daily_stock(symbol, outputsize=ADJUSTED, datatype=DATATYPE):
         return pandas_read_csv(
             f"{URL_BASE}function=TIME_SERIES_DAILY&symbol={symbol}&outputsize={outputsize}&datatype={datatype}&apikey={API_KEY}"
         )
 
     @staticmethod
-    def get_daily_adjusted_request(symbol, outputsize="compact", datatype=DATATYPE):
+    def get_daily_adjusted_request(symbol, outputsize=ADJUSTED, datatype=DATATYPE):
         return pandas_read_csv(
             f"{URL_BASE}function=TIME_SERIES_DALY_ADJUSTED&symbol={symbol}&outputsize={outputsize}&datatype={datatype}&apikey={API_KEY}"
         )
@@ -102,7 +104,7 @@ class AlphaVantage(Static):
     """Alpha Intelligence end"""
 
     @staticmethod
-    def get_stock_data(symbol, interval, datatype=DATATYPE, outputsize="full"):
+    def get_stock_data(symbol, interval, datatype=DATATYPE, outputsize=OUTPUTSIZE):
         function = "TIME_SERIES_DAILY" if interval == "1day" else "TIME_SERIES_INTRADAY_EXTENDED"
         url = f"{URL_BASE}function={function}&symbol={symbol}&apikey={API_KEY}&datatype={datatype}&outputsize={outputsize}"
         if interval != "1day": url += f"&interval={interval}"
