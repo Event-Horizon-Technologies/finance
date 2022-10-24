b86f8f2b76703782ad074534c8968b36b42ff4e5
Dillon-Roller <dillonroller.18@gmail.com>
2022-10-23 23:24:56 -0400

    Add return types for Asset

--- finance/Asset.py
+++ finance/Asset.py
@@ -2,10 +2,11 @@ from finance.HistoricalData import HistoricalData
 from finance import Utils
 
 import yfinance as yf
+from numpy import ndarray
 
 class Asset:
     def __init__(self, symbol, history=None, timeframe="1d",
-                 length=None, auto_adjust=True, start_date=None, end_date=None):
+                 length=None, auto_adjust=True, start_date=None, end_date=None) -> None:
         self.symbol = symbol
         self.timeframe = timeframe
         self.length = Utils.MAX[timeframe] if length is None else length
@@ -31,19 +32,19 @@ class Asset:
     def __get_history(self):
         return Asset.get_history(self.symbol, self.timeframe, self.length, self.auto_adjust)
 
-    def __create_historical_data(self, interval="1d", price_type="Close"):
+    def __create_historical_data(self, interval="1d", price_type="Close") -> HistoricalData:
         # the yfinance API can have bad data in the first and last row of history for some reason, thus the '[1:-1]'
         series = self.history[price_type][1:-1]
         return HistoricalData(series=series, interval=interval, start_date=self.start_date, end_date=self.end_date)
 
-    def get_price_by_date(self, date):
+    def get_price_by_date(self, date) -> float:
         return self.close.get_val_by_date(date)
 
-    def dollar_cost_average(self, period):
+    def dollar_cost_average(self, period) -> float:
         return (self.close.values[-1] / self.close.values[::period]).mean()
 
-    def lump_sum(self):
+    def lump_sum(self) -> float:
         return self.close.values[-1] / self.close.values[0]
 
-    def plot(self, shares=1, show=True):
+    def plot(self, shares=1, show=True) -> None:
         (self.close * shares).plot(label=self.symbol, show=show)
