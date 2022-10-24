21f8c5394b7cd5ff53cbffa231970b3e4e18811c
Dillon-Roller <dillonroller.18@gmail.com>
2022-10-23 22:36:52 -0400

    Add return types for Trainer.py

--- finance/Trainer.py
+++ finance/Trainer.py
@@ -10,16 +10,16 @@ from keras import activations, callbacks, layers, losses, models, optimizers
 from pathlib import Path
 
 class Trainer:
-    def __init__(self):
+    def __init__(self) -> None:
         self.input_train = self.target_train = self.input_test = self.target_test = self.model = None
         self.input_means = self.target_mean = 0.0
         self.input_stds = self.target_std = 1.0
 
     @property
-    def input_size(self):
+    def input_size(self) -> int:
         return self.input_train.shape[1] if self.input_train is not None else 0
 
-    def __standardize(self):
+    def __standardize(self) -> None:
         self.input_means = np.apply_along_axis(np.mean, 0, np.concatenate((self.input_train, self.input_test)))
         self.target_mean = np.mean(np.concatenate((self.target_train, self.target_test)))
 
@@ -37,18 +37,18 @@ class Trainer:
         self.target_test /= self.target_std
 
     @staticmethod
-    def __create_assets(symbols, timeframe="1d"):
+    def __create_assets(symbols, timeframe="1d") -> list[Asset]:
         return [Asset(symbol=symbol, timeframe=timeframe) for symbol in symbols]
 
     @staticmethod
-    def create_pickled_assets(symbols, timeframe, pickle_path):
+    def create_pickled_assets(symbols, timeframe, pickle_path) -> None:
         with open(pickle_path, "wb") as f:
             pickle.dump(Trainer.__create_assets(symbols, timeframe), f)
 
-    def save_model(self, path):
+    def save_model(self, path) -> None:
         self.model.save(path)
 
-    def create_model(self, hidden_layers=16, width=32, activation=activations.selu, dropout=0.5):
+    def create_model(self, hidden_layers=16, width=32, activation=activations.selu, dropout=0.5) -> None:
         self.model = models.Sequential()
 
         self.model.add(layers.Dense(units=width, input_shape=(self.input_size,), activation=activation))
@@ -62,7 +62,7 @@ class Trainer:
         self.model.compile(optimizer=optimizers.Adam(), loss=losses.MeanSquaredError())
 
     def generate_data(self, saved_assets=None, symbols=None, timeframe="1d",
-                      indicators=None, prediction_offset=30, validation_split=0.1):
+                      indicators=None, prediction_offset=30, validation_split=0.1) -> None:
         if not (saved_assets or symbols):
             raise Exception("No assets to generate data with. Must provide either 'symbols' or 'saved_assets'")
         if not indicators:
@@ -96,14 +96,14 @@ class Trainer:
 
         self.__standardize()
 
-    def train(self, batch_size=sys.maxsize, epochs=sys.maxsize, patience=3):
+    def train(self, batch_size=sys.maxsize, epochs=sys.maxsize, patience=3) -> None:
         self.model.fit(x=self.input_train, y=self.target_train, validation_data=(self.input_test, self.target_test),
                        batch_size=batch_size, epochs=epochs, callbacks=callbacks.EarlyStopping(patience=patience))
 
-    def load_data(self, path_to_data):
+    def load_data(self, path_to_data) -> None:
         with open(path_to_data, "rb") as f:
             self.__dict__ = pickle.load(f)
 
-    def save_data(self, path_to_data):
+    def save_data(self, path_to_data) -> None:
         with open(path_to_data, "wb") as f:
             pickle.dump(self.__dict__, f)
